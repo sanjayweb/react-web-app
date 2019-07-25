fa044359f78b8b8be80093489257b9eccc86eb60
Sanjay <sanjaysaha86@gmail.com>
2019-07-25 22:07:39 +0530

    react web app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} alt="logo" />        
+      </header>
+	  <section>ghgh</section>
+	  <footer>fgghgh</footer>
+    </div>
+  );
+}
+
+export default App;
